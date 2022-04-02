5572ee1846c97b28cdbd3498c7c95f301ca233d1
ZacharyJSwatfigure <zacharyswatfigure@gmail.com>
2022-04-02 13:57:23 -0600

    api updates

---
+++ src/components/PracticeAPI.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import axios from 'axios';
+import '../style/addWorkout.css'
+
+const baseURL = `https://wger.de/api/v2/exerciseinfo/?language=2`;
+
+export default function WorkoutList() {
+    const [posts, setPosts] = React.useState([]);
+
+    React.useEffect(() => {
+        const findWorkout = async () => {
+            await axios.get(baseURL).then((response) => {
+                setPosts(response.data.results);
+            }).catch(err => console.error(err));
+        }
+        findWorkout()
+    }, []);
+
+    if (!posts) return null;
+
+    return (
+        <div>
+            {posts.map((post, index) => (
+                <div className='workoutBox' key={post.id.toString() + index}>
+                    <h1>{post.name}</h1>
+                    <p>{post.description}</p>
+                    <p>{post.category.name}</p>
+                    <button className='addToWorkout'>Add to Workout</button>
+                </div>
+            ))}
+        </div>
+
+    );
+}
