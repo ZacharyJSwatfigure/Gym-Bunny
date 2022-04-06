3dbd9f9556640d18ef17bd4d578ce15dc9a32510
ZacharyJSwatfigure <zacharyswatfigure@gmail.com>
2022-04-05 18:55:24 -0600

    updates

--- src/components/PracticeAPI.js
+++ src/components/PracticeAPI.js
@@ -2,33 +2,57 @@ import React from 'react';
 import axios from 'axios';
 import '../style/addWorkout.css'
 
-const baseURL = `https://wger.de/api/v2/exerciseinfo/?language=2`;
+let userInput = '';
+
+
+// let baseURL = `https://wger.de/api/v2/exerciseinfo/?limit=50/?language=2&category&id=10`;
+
+
+
+let baseURL = `https://wger.de/api/v2/exerciseinfo/?language=2`;
+
+
 
 export default function WorkoutList() {
-    const [posts, setPosts] = React.useState([]);
-
-    React.useEffect(() => {
-        const findWorkout = async () => {
-            await axios.get(baseURL).then((response) => {
-                setPosts(response.data.results);
-            }).catch(err => console.error(err));
+    const options = {
+        method: 'GET',
+        url: 'https://exercisedb.p.rapidapi.com/exercises/bodyPartList',
+        headers: {
+            'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+            'X-RapidAPI-Key': '568d91d257msh08056a4a6e14a0ap190e88jsnda8965713d51'
         }
-        findWorkout()
-    }, []);
+    };
 
-    if (!posts) return null;
+    axios.request(options).then(function (response) {
+        console.log(response.data);
+    }).catch(function (error) {
+        console.error(error);
+    });
+    // const [posts, setPosts] = React.useState([]);
 
-    return (
-        <div>
-            {posts.map((post, index) => (
-                <div className='workoutBox' key={post.id.toString() + index}>
-                    <h1>{post.name}</h1>
-                    <p>{post.description}</p>
-                    <p>{post.category.name}</p>
-                    <button className='addToWorkout'>Add to Workout</button>
-                </div>
-            ))}
-        </div>
-        
-    );
+    // React.useEffect(() => {
+    //     const findWorkout = async () => {
+    //         await axios.get(baseURL).then((response) => {
+    //             setPosts(response.data.results);
+    //         }).catch(err => console.error(err));
+    //     }
+    //     findWorkout()
+    // }, []);
+
+    // if (!posts) return null;
+
+    // return (
+    //     <div>
+    //         {posts.map((post, index) => (
+    //             <div className='workoutBox' key={post.id.toString() + index}>
+    //                 <p>{post.name}</p>
+    //                 <h1>{post.description}</h1>
+    //                 <h1>{post.category.id}</h1>
+    //                 {/* <p>{post.category.name}</p> */}
+    //                 <button className='addToWorkout'>Add to Workout</button>
+    //             </div>
+    //         ))}
+    //     </div>
+
+    // );
 }
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import '../style/Search.css';
+import WorkoutList from '../components/PracticeAPI';
 
 export default function Search() {
     return (
-        <h1>Search</h1>
+        <form>
+            <section>
+                <h1>Body Category:
+                    <div className="dropdown">
+                        <button className="dropbtn">Dropdown</button>
+                        <div className="dropdown-content">
+                            <a>Abs</a>
+                            <a>Arms</a>
+                            <a>Back</a>
+                            <a>Calves</a>
+                            <a>Chest</a>
+                            <a>Legs</a>
+                            <a>Shoulders</a>
+                        </div>
+                    </div>
+                </h1>
+                <WorkoutList/>
+            </section>
+        </form>
     );
 }
+
