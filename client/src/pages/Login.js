1c502eed5543975965ef131bcc19908f08c2f92b
ZacharyJSwatfigure <zacharyswatfigure@gmail.com>
2022-04-07 21:26:29 -0600

    sending to back end in proggress

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -5,9 +5,9 @@ import { LOGIN } from '../graphql/mutations/login';
 import '../style/Login.css';
 
 export default function Login() {
-    
-    const [ username, setUsername] = useState('')
-    const [ password, setPassword] = useState('')
+
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
 
     const [loginMutation,] = useMutation(LOGIN);
     const newUser = '';
@@ -19,16 +19,18 @@ export default function Login() {
                 <h1 className="greeting">Login</h1>
             </section>
 
-            <section className = 'loginInfoHolder'>
+            <section className='loginInfoHolder'>
 
-                <input className = 'userInput' type="Text" placeholder='Username' onChange = {event=>setUsername(event.target.value)} />
+                <input className='userInput' type="Text" placeholder='Username' onChange={event => setUsername(event.target.value)} />
 
-                <input className = 'userInput' type="Password" placeholder='Password' onChange = {event=>setPassword(event.target.value)} />
+                <input className='userInput' type="Password" placeholder='Password' onChange={event => setPassword(event.target.value)} />
 
-                <button className = 'loginBtn' type='submit' onClick={ (username, password, newUser) => {
-                    return (
-                        console.log(username + password)
-                    );
+                <button className='loginBtn' type='submit' onClick={(username, password, newUser) => {
+                    newUser = {
+                        username,
+                        password
+                    };
+
                 }} >Log In</button>
 
             </section>
