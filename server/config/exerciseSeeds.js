50b423d8018ba8842d2f5b11a42f0dab3cbde0f6
Najla Parker <najla08@gmail.com>
2022-04-11 17:12:12 -0600

    created exercise seeds

---
+++ server/config/exerciseSeeds.js
@@ -0,0 +1,102 @@
+[
+  {
+      "name":"Crunches",
+      "exercise_Id": 1,
+      "focus": abs
+  },
+  {
+      "name":"Sit ups",
+      "exercise_Id": 1,
+      "focus": abs
+  },
+  {
+      "name":"Russian Twists",
+      "exercise_Id": 1,
+      "focus": abs
+  },
+  {
+      "name":"Plank hold",
+      "exercise_Id": 1,
+      "focus": abs
+  },
+  {
+      "name":"Farmer's Walk",
+      "exercise_Id": 1,
+      "focus": abs
+  },
+  {
+      "name":"Knee Raises",
+      "exercise_Id": 1,
+      "focus": abs
+  },
+  {
+      "name":"Side Bends",
+      "exercise_Id": 1,
+      "focus": abs
+  },
+  {
+      "name":"Barbell ab rollouts",
+      "exercise_Id": 1,
+      "focus": abs
+  },
+  {
+      "name":"Cable Crunches",
+      "exercise_Id": 1,
+      "focus": abs
+  },
+  {
+      "name":"Leg Raises",
+      "exercise_Id": 1,
+      "focus": abs
+  },
+  {
+      "name":"Side plank hold",
+      "exercise_Id": 1,
+      "focus": abs
+  },
+  {
+      "name":"Exercise ball pull in",
+      "exercise_Id": 1,
+      "focus": abs
+  },
+  {
+      "name":"Spider crawl",
+      "exercise_Id": 1,
+      "focus": abs
+  },
+  {
+      "name":"Cocoons",
+      "exercise_Id": 1,
+      "focus": abs
+  },
+  {
+      "name":"Suspended ab fall-out",
+      "exercise_Id": 1,
+      "focus": abs
+  },
+  {
+      "name":"Landmine twist",
+      "exercise_Id": 1,
+      "focus": abs
+  },
+  {
+      "name":"Dumbbell side bend",
+      "exercise_Id": 1,
+      "focus": abs
+  },
+  {
+      "name":"Sledgehammer swing",
+      "exercise_Id": 1,
+      "focus": abs
+  },
+  {
+      "name":"V-up",
+      "exercise_Id": 1,
+      "focus": abs
+  },
+  {
+      "name":"Standing Cable Wood Chop",
+      "exercise_Id": 1,
+      "focus": abs
+  },
+]
