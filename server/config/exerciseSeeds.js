11acb5dd137f6f1c69ff86fc36c901e98a8a8c7e
Najla Parker <najla08@gmail.com>
2022-04-11 17:26:57 -0600

    created exercise seeds arms

--- server/config/exerciseSeeds.js
+++ server/config/exerciseSeeds.js
@@ -1,102 +1,202 @@
 [
   {
-      "name":"Crunches",
-      "exercise_Id": 1,
-      "focus": abs
+    "name": "Crunches",
+    "focusId": 1,
+    "focus": abs
   },
   {
-      "name":"Sit ups",
-      "exercise_Id": 1,
-      "focus": abs
+    "name": "Sit ups",
+    "focusId": 1,
+    "focus": abs
   },
   {
-      "name":"Russian Twists",
-      "exercise_Id": 1,
-      "focus": abs
+    "name": "Russian Twists",
+    "focusId": 1,
+    "focus": abs
   },
   {
-      "name":"Plank hold",
-      "exercise_Id": 1,
-      "focus": abs
+    "name": "Plank hold",
+    "focusId": 1,
+    "focus": abs
   },
   {
-      "name":"Farmer's Walk",
-      "exercise_Id": 1,
-      "focus": abs
+    "name": "Farmer's Walk",
+    "focusId": 1,
+    "focus": abs
   },
   {
-      "name":"Knee Raises",
-      "exercise_Id": 1,
-      "focus": abs
+    "name": "Knee Raises",
+    "focusId": 1,
+    "focus": abs
   },
   {
-      "name":"Side Bends",
-      "exercise_Id": 1,
-      "focus": abs
+    "name": "Side Bends",
+    "focusId": 1,
+    "focus": abs
   },
   {
-      "name":"Barbell ab rollouts",
-      "exercise_Id": 1,
-      "focus": abs
+    "name": "Barbell ab rollouts",
+    "focusId": 1,
+    "focus": abs
   },
   {
-      "name":"Cable Crunches",
-      "exercise_Id": 1,
-      "focus": abs
+    "name": "Cable Crunches",
+    "focusId": 1,
+    "focus": abs
   },
   {
-      "name":"Leg Raises",
-      "exercise_Id": 1,
-      "focus": abs
+    "name": "Leg Raises",
+    "focusId": 1,
+    "focus": abs
   },
   {
-      "name":"Side plank hold",
-      "exercise_Id": 1,
-      "focus": abs
+    "name": "Side plank hold",
+    "focusId": 1,
+    "focus": abs
   },
   {
-      "name":"Exercise ball pull in",
-      "exercise_Id": 1,
-      "focus": abs
+    "name": "Exercise ball pull in",
+    "focusId": 1,
+    "focus": abs
   },
   {
-      "name":"Spider crawl",
-      "exercise_Id": 1,
-      "focus": abs
+    "name": "Spider crawl",
+    "focusId": 1,
+    "focus": abs
   },
   {
-      "name":"Cocoons",
-      "exercise_Id": 1,
-      "focus": abs
+    "name": "Cocoons",
+    "focusId": 1,
+    "focus": abs
   },
   {
-      "name":"Suspended ab fall-out",
-      "exercise_Id": 1,
-      "focus": abs
+    "name": "Suspended ab fall-out",
+    "focusId": 1,
+    "focus": abs
   },
   {
-      "name":"Landmine twist",
-      "exercise_Id": 1,
-      "focus": abs
+    "name": "Landmine twist",
+    "focusId": 1,
+    "focus": abs
   },
   {
-      "name":"Dumbbell side bend",
-      "exercise_Id": 1,
-      "focus": abs
+    "name": "Dumbbell side bend",
+    "focusId": 1,
+    "focus": abs
   },
   {
-      "name":"Sledgehammer swing",
-      "exercise_Id": 1,
-      "focus": abs
+    "name": "Sledgehammer swing",
+    "focusId": 1,
+    "focus": abs
   },
   {
-      "name":"V-up",
-      "exercise_Id": 1,
-      "focus": abs
+    "name": "V-up",
+    "focusId": 1,
+    "focus": abs
   },
   {
-      "name":"Standing Cable Wood Chop",
-      "exercise_Id": 1,
-      "focus": abs
+    "name": "Standing Cable Wood Chop",
+    "focusId": 1,
+    "focus": abs
+  },
+  {
+    "name": "Barbell bicep curl",
+    "focusId": 2,
+    "focus": arms,
+  },
+  {
+    "name": "Rows",
+    "focusId": 2,
+    "focus": arms,
+  },
+  {
+    "name": "Bench",
+    "focusId": 2,
+    "focus": arms,
+  },
+  {
+    "name": "Rickshaw Carry",
+    "focusId": 2,
+    "focus": arms,
+  },
+  {
+    "name": "Close-grip bench press",
+    "focusId": 2,
+    "focus": arms,
+  },
+  {
+    "name": "Tricep kickbacks",
+    "focusId": 2,
+    "focus": arms,
+  },
+  {
+    "name": "Standing overhead extension",
+    "focusId": 2,
+    "focus": arms,
+  },
+  {
+    "name": "Seated overhead extension",
+    "focusId": 2,
+    "focus": arms,
+  },
+  {
+    "name": "Dips",
+    "focusId": 2,
+    "focus": arms,
+  },
+  {
+    "name": "Hammer curls",
+    "focusId": 2,
+    "focus": arms,
+  },
+  {
+    "name": "Concentration curl",
+    "focusId": 2,
+    "focus": arms,
+  },
+  {
+    "name": "Cable bicep curl",
+    "focusId": 2,
+    "focus": arms,
+  },
+  {
+    "name": "Preacher Curl",
+    "focusId": 2,
+    "focus": arms,
+  },
+  {
+    "name": "Seated triceps press",
+    "focusId": 2,
+    "focus": arms,
+  },
+  {
+    "name": "Wrist Roller",
+    "focusId": 2,
+    "focus": arms,
+  },
+  {
+    "name": "Zottman Curl",
+    "focusId": 2,
+    "focus": arms,
+  },
+  {
+    "name": "Seated finger curl",
+    "focusId": 2,
+    "focus": arms,
+  },
+  {
+    "name": "Standing One-Arm Cable Curl",
+    "focusId": 2,
+    "focus": arms,
+  },
+  {
+    "name": "Bench Press - Powerlifting",
+    "focusId": 2,
+    "focus": arms,
+  },
+  {
+    "name": "Palms-Up Dumbbell Wrist Curl Over A Bench",
+    "focusId": 2,
+    "focus": arms,
   },
 ]
