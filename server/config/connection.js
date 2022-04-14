ad25bc51569c2c9c0adc6b6244e13732cfb4d96d
Giovanni <gdomino01@gmail.com>
2022-04-14 14:56:29 -0600

    Readjusted the connection.js and seeds.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,10 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://localhost:27017/gym-bunny', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+const mongoUri = "mongodb://localhost:27017/gym-bunny";
+
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
 module.exports = mongoose.connection;
