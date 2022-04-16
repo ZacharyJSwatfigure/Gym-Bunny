824b7f0befef412e2674deed4e58495f24cb52ec
ZacharyJSwatfigure <zacharyswatfigure@gmail.com>
2022-04-16 12:17:57 -0600

    styling

--- client/src/components/Exercise.jsx
+++ client/src/components/Exercise.jsx
@@ -1,15 +1,16 @@
 import React from "react";
+import '../style/Exercise.css';
 
 export const Exercise = ({ exercise, addToWorkout }) => {
   return (
-    <div>
-      <h2>{exercise.name}</h2>
-      <span
+    <div className = 'container'>
+      <h2 className = 'title'><p className = 'description'>{exercise.name}</p></h2>
+      <span className = 'exerciseSpan'
         onClick={() => {
           addToWorkout(exercise);
         }}
       >
-        <button>Add to workout</button>
+        <button className= 'addBtn'>Add to workout</button>
       </span>
     </div>
   );
