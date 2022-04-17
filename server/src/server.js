27084359c45fcec5e2f360e35b4762db9217cacd
Najla Parker <najla08@gmail.com>
2022-04-17 10:12:58 -0600

    Troubleshooting seeds

--- server/src/server.js
+++ server/src/server.js
@@ -1,50 +1,57 @@
-const express = require('express');
-const { ApolloServer } = require('apollo-server-express');
-const jwt = require('jsonwebtoken');
-const utils = require('./utils/auth');
-const { resolvers, typeDefs, } = require('./schemas');
-const db = require('./config/connection');
-const PORT = process.env.PORT || 3001;
-const app = express();
+const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
+const jwt = require("jsonwebtoken");
+const utils = require("./utils");
+const { resolvers, typeDefs } = require("./schemas");
+const db = require("./config/connection");
+const { User, Token } = require("./models");
 const path = require('path');
+
 const server = new ApolloServer({
   resolvers,
   typeDefs,
-  // context: ({req, res}) => {
-  // 	const token = req.headers.authorization;
+  context: async ({ req }) => {
+    let token;
 
-  // 	// if no token no user is logged in
-  // 	if (token.length === 0) {
-  // 		return req;
-  // 	}
+    if (req.headers.authorization) {
+      token = req.headers.authorization.split(" ").pop().trim();
+    }
 
-  // 	try {
-  // 		const {data} = jwt.verify(token, utils.secret);
-  // 		req.user = data;
-  // 	} catch (e) {
-  // 		return {error: 'Invalid token'};
-  // 	}
+    if (!token) {
+      return req;
+    }
+    const blackListedToken = await Token.findOne({ token });
+    if (blackListedToken) {
+      return req;
+    }
 
-  // 	return {
-  // 		req,
-  // 	};
-  // },
+    try {
+      const { data } = jwt.verify(token, utils.secret);
+      req.user = data;
+      req.user.token = token;
+    } catch (e) {
+      return { error: "Invalid token" };
+    }
+    return req;
+  },
 });
 
-
+const PORT = process.env.PORT || 3001;
+const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Serve up static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 
-db.once('open', async () => {
+db.once("open", async () => {
   await server.start();
-  // creates a /graphql endpoint for our server
+
   server.applyMiddleware({ app });
-  app.listen(PORT, () => console.log('Server running on PORT 3001'));
-  console.log(`graphql at http://localhost:${PORT}${server.graphqlPath}`)
+  app.listen(PORT, () => console.log("Server running on PORT 3001"));
 });
