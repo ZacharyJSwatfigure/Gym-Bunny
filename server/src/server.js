c11dfa40f39e1b9765c88d8bcec1aeab70795654
Najla Parker <najla08@gmail.com>
2022-04-17 10:03:13 -0600

    Troubleshooting seeds

--- server/src/server.js
+++ server/src/server.js
@@ -1,57 +1,42 @@
-const express = require("express");
-const { ApolloServer } = require("apollo-server-express");
-const jwt = require("jsonwebtoken");
-const utils = require("./utils");
-const { resolvers, typeDefs } = require("./schemas");
-const db = require("./config/connection");
-const { User, Token } = require("./models");
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
+const { authMiddleware } = require('./utils');
 
-const server = new ApolloServer({
-  resolvers,
-  typeDefs,
-  context: async ({ req }) => {
-    let token;
-
-    if (req.headers.authorization) {
-      token = req.headers.authorization.split(" ").pop().trim();
-    }
-
-    if (!token) {
-      return req;
-    }
-    const blackListedToken = await Token.findOne({ token });
-    if (blackListedToken) {
-      return req;
-    }
-
-    try {
-      const { data } = jwt.verify(token, utils.secret);
-      req.user = data;
-      req.user.token = token;
-    } catch (e) {
-      return { error: "Invalid token" };
-    }
-    return req;
-  },
-});
+const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
 
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
+  app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
-db.once("open", async () => {
-  await server.start();
-
-  server.applyMiddleware({ app });
-  app.listen(PORT, () => console.log("Server running on PORT 3001"));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
+
+// Create a new instance of an Apollo server with the GraphQL schema
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
+
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    })
+  })
+};
+
+// Call the async function to start the server
+startApolloServer(typeDefs, resolvers);
