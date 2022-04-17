7f396c6bfad7be5656557d86900ff38eacb0af1b
Najla Parker <najla08@gmail.com>
2022-04-16 19:34:56 -0600

    Deploying

--- server/src/server.js
+++ server/src/server.js
@@ -46,10 +46,6 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
-});
-
 db.once("open", async () => {
   await server.start();
 
