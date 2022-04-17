b6fe043fe638a6245609139bf70a08bbd6ccfb3b
Najla Parker <najla08@gmail.com>
2022-04-17 09:18:34 -0600

    Troubleshooting seeds

--- server/src/config/connection.js
+++ server/src/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGO_URI || 'mongodb+srv://nparker:xCtczppFN9IT7hZV@gym-bunny.chwem.mongodb.net/gym-bunny?retryWrites=true&w=majority',
+  process.env.MONGO_URI || 'mongodb://localhost:27017/gym-bunny',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
